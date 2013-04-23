8f7cc91a37a950277f90a77c918d36497485dbf8
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-22 22:43:37 -0700

    Adding initial files for pa2

--- pa/pa1/sploits/sploit3.c
+++ pa/pa1/sploits/sploit3.c
@@ -5,7 +5,24 @@
 #include "shellcode.h"
 
 #define TARGET "/tmp/target3"
-#define STR_SIZE 20028
+#define EGG_SIZE 20028 /* size of exploit string, enough to overrun the 256 buffer in target1.c:foo */ 
+
+/* 
+ * Main idea of exploit:
+ *     The main idea of is to exploit an integer
+ *     overflow vulnerability in the line:
+ *       count = (int)strtoul(argv[1], &in, 10);
+ *     of target3.c. Note strtoul reads an unsigned
+ *     long from a string of characters. We arrange
+ *     our input so that this unsigned long is very 
+ *     large so that the cast to an int will overflow
+ *     causing 'count' to be negative. Thus the test:
+ *       if (count < MAX_WIDGETS) 
+ *     will pass. Finally, since memcpy takes in a size_t 
+ *     as its last argument, we will overflow buf in:
+ *       memcpy(buf, in, count * sizeof(struct widget_t));
+ *     The rest is boilerplate stack smashing. 
+ */
 
 int main(void)
 {
