5d9e938afec1fa918071d9d801f80bc741b6320e
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-05-21 20:04:01 -0700

    PA2B: sanatizing input parameters to transfer.php

--- pa/pa2/zoobar_php/transfer.php
+++ pa/pa2/zoobar_php/transfer.php
@@ -2,14 +2,22 @@
   require_once("includes/common.php"); 
   nav_start_outer("Transfer");
   nav_start_inner();
-  $user_cookie = $_COOKIE[$user->cookieName];
 
-  if($_POST['submission'] && $_POST['token'] && $_POST['token'] == md5($user_cookie)) {
-    $recipient = $_POST['recipient'];
-    $zoobars = (int) $_POST['zoobars'];
+
+  // Untrusted data. Sanatize the data that is reflected or used in SQL statements
+  $recipient = sanatize_username ($_POST['recipient']); // reflected, used in SQL query
+  $user_cookie = $_COOKIE[$user->cookieName];
+  $secret_token = md5($user_cookie); // reflected, md5 will return alphanumeric chars
+  $form_token = $_POST['token']; // not reflected or stored
+  $submission_status = $_POST['submission']; // not reflected or stored
+  $zoobars = (int) $_POST['zoobars']; // reflected, cast will sanatize
+  $php_self = $_SERVER['PHP_SELF']; // reflected TODO: sanatize?
+
+
+  if($submission_status && $form_token && $form_token == $secret_token) {
     $sql = "SELECT Zoobars FROM Person WHERE PersonID=$user->id";
     $rs = $db->executeQuery($sql);
-    $sender_balance = $rs->getValueByNr(0,0) - $zoobars;
+    $sender_balance = ((int)$rs->getValueByNr(0,0)) - $zoobars;
     $sql = "SELECT PersonID FROM Person WHERE Username='$recipient'";
     $rs = $db->executeQuery($sql);
     $recipient_exists = $rs->getValueByNr(0,0);
@@ -19,7 +27,7 @@
       $db->executeQuery($sql);
       $sql = "SELECT Zoobars FROM Person WHERE Username='$recipient'";
       $rs = $db->executeQuery($sql);
-      $recipient_balance = $rs->getValueByNr(0,0) + $zoobars;
+      $recipient_balance = ((int)$rs->getValueByNr(0,0)) + $zoobars;
       $sql = "UPDATE Person SET Zoobars = $recipient_balance " .
              "WHERE Username='$recipient'";
       $db->executeQuery($sql);
@@ -30,11 +38,11 @@
 ?>
 <p><b>Balance:</b>
 <span id="myZoobars"></span> zoobars</p>
-<form method="POST" name="transferform" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-<p>Send <input name="zoobars" type="text" value="<?php echo $_POST['zoobars']; ?>" size="5"> zoobars</p>
-<p>to <input name="recipient" type="text" value="<?php echo $_POST['recipient']; ?>"></p>
+<form method="POST" name="transferform" action="<?php echo $php_self; ?>">
+<p>Send <input name="zoobars" type="text" value="<?php echo $zoobars; ?>" size="5"> zoobars</p>
+<p>to <input name="recipient" type="text" value="<?php echo $recipient; ?>"></p>
 <input type="submit" name="submission" value="Send">
-<input type="text" name="token" value="<?php echo md5($user_cookie); ?>" style="display:none">
+<input type="text" name="token" value="<?php echo $secret_token; ?>" style="display:none">
 </form>
 <span class="warning"><?php echo $result; ?></span>
 <?php 
