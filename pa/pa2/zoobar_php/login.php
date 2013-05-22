a978f5763d7cdf7221fe98ce71142f59b2a241d0
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-05-21 20:15:16 -0700

    PA2B: sanitizing inputs in login.php. Attack E should be dwarfed.

--- pa/pa2/zoobar_php/login.php
+++ pa/pa2/zoobar_php/login.php
@@ -7,11 +7,15 @@ function validate_registration(&$user) {
   $success = false;
   $username = $_POST['login_username'];
   $password = $_POST['login_password'];
+  $sanatized_username = sanatize_username ($username);
+
   if(!$username) 
     $login_error = "You must supply a username to register.";
+  else if(strlen ($username) != strlen($sanatized_username))
+    $login_error = "Usernames can only contain alphanumeric characters and the characters \"-\" and \"_\". Please try again. ";
   else if(!$password) 
     $login_error = "You must supply a password to register.";
-  else if(!$user->_addRegistration($username, $password)) 
+  else if(!$user->_addRegistration($sanatized_username, $password)) 
     $login_error = "Registration failed.";
   else $success = true;
   return $success;
@@ -23,11 +27,15 @@ function validate_login(&$user) {
   $success = false;
   $username = $_POST['login_username'];
   $password = $_POST['login_password'];
+  $sanatized_username = sanatize_username ($username);
+  
   if(!$username) 
     $login_error = "You must supply a username to log in.";
+  else if(strlen ($username) != strlen($sanatized_username))
+    $login_error = "Usernames can only contain alphanumeric characters and the characters \"-\" and \"_\". Please try again. ";
   else if(!$password) 
     $login_error = "You must supply a password to log in.";
-  else if(!$user->_checkLogin($username, $password)) 
+  else if(!$user->_checkLogin($sanatized_username, $password)) 
     $login_error = "Invalid username or password.";
   else $success = true;
   return $success;
@@ -51,20 +59,21 @@ function validate_user(&$user)
 function display_login()
 {
   nav_start_outer("Login");
+  $login_username = sanatize_username ($_POST['login_username']);
+  $php_self = $_SERVER['PHP_SELF'];
 ?>
 <div id="login" class="centerpiece">
-<form name=loginform method=POST action="<?php echo $_SERVER['PHP_SELF']?>">
+<form name="loginform" method="POST" action="<?php echo $php_self; ?>">
 <table>
 <tr>
    <td>Username:</td>
-  <td><input type=text name=login_username size=30 autocomplete=no value=<?php 
-    echo htmlspecialchars($_POST['login_username']); ?>></td>
+  <td><input type="text" name="login_username" size="30" autocomplete="no" value="<?php echo $login_username; ?>"></td>
 </tr>
 <tr>
    <td>Password:</td>
-  <td colspan=2><input type=password name=login_password size=30 autocomplete=no>
-  <input type=submit name=submit_login value="Log in">
-  <input type=submit name=submit_registration value="Register"></td>
+  <td colspan="2"><input type="password" name="login_password" size="30" autocomplete="no">
+  <input type="submit" name="submit_login" value="Log in">
+  <input type="submit" name="submit_registration" value="Register"></td>
 </tr>
 </table>
 </form>
